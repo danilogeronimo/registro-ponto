cc2f7aa24d45933346047cf85828953dc5d63a89
Danilo Geronimo <danilo.geronimo@hotmail.com>
2021-07-20 23:13:09 -0300

    Primeiro commit

---
+++ src/main/java/com/dio/registroponto/model/Localidade.java
@@ -0,0 +1,20 @@
+package com.dio.registroponto.model;
+
+import lombok.*;
+
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class Localidade {
+    @Id
+    private long id;
+    @ManyToOne
+    private NivelAcesso nivelAcesso;
+    private String descricao;
+}
---
+++ src/main/java/com/dio/registroponto/model/Movimentacao.java
@@ -0,0 +1,42 @@
+package com.dio.registroponto.model;
+
+import lombok.*;
+
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+@Entity
+public class Movimentacao {
+
+    //quando há chaves primárias compostas, cria se uma classe implementando Serielizable (?) e use se a notação Embeddable para referenciar as  chaves na classe principal "Movimentacao" usando @EmbeddedId
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @EqualsAndHashCode
+    @Embeddable
+    public class MovimentacaoId implements Serializable{
+        private Long idMovimento;
+        private Long idUsuario;
+    }
+
+    @EmbeddedId
+    private MovimentacaoId id;
+    private LocalDateTime dataEntrada;
+    private LocalDateTime dataSaida;
+    private BigDecimal periodo;
+    @ManyToOne
+    private Ocorrencia ocorrencia;
+    @ManyToOne
+    private Calendario calendario;
+
+}
